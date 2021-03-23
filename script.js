a69d27edc4c986c42c68dda147a5b17c2c8e88b4
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-03-23 11:16:04 -0300

    Index, Style and Script files finished.

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Oi, dev curioso! Eu vi que você veio ver o código, haha!");
+
+console.log(
+  "Acho que vai ser mais fácil se você conferir o código completo no meu Github: https://github.com/lyssacavalcanti =)"
+);
